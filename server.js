d81d5a0a0d45206ef21737123021ecf835394001
gxranas <g.x.ranas@gmail.com>
2023-06-23 12:20:25 +0800

    Replace cors

--- server.js
+++ server.js
@@ -18,13 +18,12 @@ app.use(cookieSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(express.json());
 app.use(cors({
     origin: process.env.CLIENT_URL,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
 }));
-
-app.use(express.json());
 app.use("/auth", UsersRouter);
 
 mongoose.connect(process.env.CONNECTION_STRING);
